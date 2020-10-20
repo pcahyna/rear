8270599098c5093a964a56036fb0c14dffb27f01
Vitezslav Crhonek <vcrhonek@redhat.com>
2020-10-20 13:08:37 +0200

    Add initial support for LUKS2
    Use 'blkid' to check that it's LUKS and to find its version. It should
    be prefered over 'cryptsetup' according to LUKS maintainers.

--- usr/share/rear/layout/save/GNU/Linux/260_crypt_layout.sh
+++ usr/share/rear/layout/save/GNU/Linux/260_crypt_layout.sh
@@ -30,24 +30,45 @@ while read target_name junk ; do
         source_device="$(get_device_name ${slave##*/})"
     done
 
-    if ! cryptsetup isLuks $source_device >/dev/null 2>&1; then
+    if ! blkid -p -o export $source_device >$TMP_DIR/blkid.output ; then
+        LogPrintError "Error: Cannot get attributes for $target_name ('blkid -p -o export  $source_device' failed)"
         continue
     fi
 
-    # gather crypt information
-    cipher=$(cryptsetup luksDump $source_device | grep "Cipher name" | sed -r 's/^.+:\s*(.+)$/\1/')
-    mode=$(cryptsetup luksDump $source_device | grep "Cipher mode" | cut -d: -f2- | awk '{printf("%s",$1)};')
-    key_size=$(cryptsetup luksDump $source_device | grep "MK bits" | sed -r 's/^.+:\s*(.+)$/\1/')
-    hash=$(cryptsetup luksDump $source_device | grep "Hash spec" | sed -r 's/^.+:\s*(.+)$/\1/')
-    uuid=$(cryptsetup luksDump $source_device | grep "UUID" | sed -r 's/^.+:\s*(.+)$/\1/')
-    keyfile_option=$([ -f /etc/crypttab ] && awk '$1 == "'"$target_name"'" && $3 != "none" && $3 != "-" && $3 != "" { print "keyfile=" $3; }' /etc/crypttab)
+    if ! grep -q "TYPE=crypto_LUKS" $TMP_DIR/blkid.output ; then
+        Log "Skipping $target_name (its $source_device is not a LUKS device)"
+        continue
+    fi
 
-    # LUKS version 2 is not yet suppported, see https://github.com/rear/rear/issues/2204
-    # When LUKS version 2 is used the above code fails at least to determine the hash value
-    # so we use an empty hash value as a simple test if gathering crypt information was successful:
-    test "$hash" || Error "No hash value for LUKS device '$target_name' at '$source_device' (only LUKS version 1 is supported)"
+    # Detect LUKS version
+    version=$( grep "VERSION" $TMP_DIR/blkid.output | cut -d= -f2 )
+    if !( test $version = "1" || test $version = "2") ; then
+        LogPrintError "Error: Unsupported LUKS version for $target_name"
+        continue
+    fi
+    luks_type=luks$version
 
-    echo "crypt /dev/mapper/$target_name $source_device cipher=$cipher-$mode key_size=$key_size hash=$hash uuid=$uuid $keyfile_option" >> $DISKLAYOUT_FILE
+    # Gather crypt information:
+    if ! cryptsetup luksDump $source_device >$TMP_DIR/cryptsetup.luksDump ; then
+        LogPrintError "Error: Cannot get LUKS values for $target_name ('cryptsetup luksDump $source_device' failed)"
+        continue
+    fi
+    uuid=$( grep "UUID" $TMP_DIR/cryptsetup.luksDump | sed -r 's/^.+:\s*(.+)$/\1/' )
+    keyfile_option=$( [ -f /etc/crypttab ] && awk '$1 == "'"$target_name"'" && $3 != "none" && $3 != "-" && $3 != "" { print "keyfile=" $3; }' /etc/crypttab )
+    if test $luks_type = "luks1" ; then
+        cipher_name=$( grep "Cipher name" $TMP_DIR/cryptsetup.luksDump | sed -r 's/^.+:\s*(.+)$/\1/' )
+        cipher_mode=$( grep "Cipher mode" $TMP_DIR/cryptsetup.luksDump | cut -d: -f2- | awk '{printf("%s",$1)};' )
+        cipher=$cipher_name-$cipher_mode
+        key_size=$( grep "MK bits" $TMP_DIR/cryptsetup.luksDump | sed -r 's/^.+:\s*(.+)$/\1/' )
+        hash=$( grep "Hash spec" $TMP_DIR/cryptsetup.luksDump | sed -r 's/^.+:\s*(.+)$/\1/' )
+    elif test $luks_type = "luks2" ; then
+        cipher=$( grep "cipher:" $TMP_DIR/cryptsetup.luksDump | sed -r 's/^.+:\s*(.+)$/\1/' )
+        key_size=$( grep "Cipher key" $TMP_DIR/cryptsetup.luksDump | sed -r 's/^.+:\s*(.+) bits$/\1/' )
+        hash=$( grep "Hash" $TMP_DIR/cryptsetup.luksDump | sed -r 's/^.+:\s*(.+)$/\1/' )
+    fi
+
+    echo "crypt /dev/mapper/$target_name $source_device type=$luks_type cipher=$cipher key_size=$key_size hash=$hash uuid=$uuid $keyfile_option" >> $DISKLAYOUT_FILE
+
 done < <( dmsetup ls --target crypt )
 
 # cryptsetup is required in the recovery system if disklayout.conf contains at least one 'crypt' entry
