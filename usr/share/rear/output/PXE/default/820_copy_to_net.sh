4eac06dc7e8d512ae5e5a143826685ae10db0e69
Pavel Cahyna <pcahyna@redhat.com>
2022-08-05 17:21:20 +0200

    Remove the lftp part of restored 820_copy_to_net.sh
    This job is being done by the generic code, so the output files would be
    transferred twice.

--- usr/share/rear/output/PXE/default/820_copy_to_net.sh
+++ usr/share/rear/output/PXE/default/820_copy_to_net.sh
@@ -14,19 +14,8 @@ case "$scheme" in
         return 0
         ;;
     (fish|ftp|ftps|hftp|http|https|sftp)
-        LogPrint "Transferring PXE files to $OUTPUT_URL"
-        for result_file in "${RESULT_FILES[@]}" ; do
-            path=$(url_path $OUTPUT_URL)
-
-            # Make sure that destination directory exists, otherwise lftp would copy
-            # RESULT_FILES into last available directory in the path.
-            # e.g. OUTPUT_URL=sftp://<host_name>/iso/server1 and have "/iso/server1"
-            # directory missing, would upload RESULT_FILES into sftp://<host_name>/iso/
-            lftp -c "$OUTPUT_LFTP_OPTIONS; open $OUTPUT_URL; mkdir -fp ${path}"
-
-            LogPrint "Transferring file: $result_file"
-            lftp -c "$OUTPUT_LFTP_OPTIONS; open $OUTPUT_URL; mput $result_file" || Error "lftp failed to transfer '$result_file' to '$OUTPUT_URL' (lftp exit code: $?)"
-        done
+        # output/default/950_copy_result_files.sh will transfer them
+        return 0
         ;;
     (rsync)
         LogPrint "Transferring PXE files to $OUTPUT_URL"
